351824bc119a835222fb87e6d3fccb3f89557e2c
nisseBD <70211737+nisseBD@users.noreply.github.com>
2021-04-21 15:46:05 +0200

    init

---
+++ UserModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    user: String,
+    password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+exports.saveUser = function (inUser, inPassword) {
+    var user = new User({
+        user: inUser,
+        password: inPassword,
+    });
+    user.save();
+};
+
+exports.getUser = async function (Uuser) {
+    return await User.findOne({ user: Uuser });
+}
--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-const express = require('express')
+const express = require('express');
+const { get } = require('mongoose');
+const bcrypt = require('bcryptjs');
+const UserModel = require("./UserModel");
+const databaseModule = require("./databaseModule.js");
 const app = express()
-const port = 3000
+const port = 6969
+const staticDir = __dirname + "\\static\\";
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(staticDir));
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.get('/', (req, res) => res.sendFile(staticDir + "html\\index.html"))
+app.get('/spel', (req, res) => res.sendFile(staticDir + "html\\spel.html"))
+app.get('/register', (req, res) => res.sendFile(staticDir + "html\\register.html"))
+app.get('/login', (req, res) => res.sendFile(staticDir + "html\\login.html"))
+
+
+
+app.post('/registerUser', async function (req, res) {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    console.log(hashedPassword);
+    UserModel.saveUser(req.body.user, hashedPassword);
+    res.redirect("/");
+})
+
+app.post('/login', async function (req, res) {
+    const user = await UserModel.getUser(req.body.user);
+    await bcrypt.compare(req.body.password, user.password, (err, success) => {
+        if (err) {
+            console.log(err);
+        }
+
+        if (success) console.log("Success");
+        else console.log("Fail");
+    });
+    res.redirect("/");
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
